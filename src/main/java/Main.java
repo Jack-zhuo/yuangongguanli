99c9a2b85dacc19d36cd0ccfeab5fb07111a174e
zhuoyue <zyjava@qq.com>
2024-11-25 17:41:52 +0800

    修改Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,17 @@
+
+
 import javax.swing.*;
 
-public static void main(String[] args) {
-    // 注册MySQL驱动
-    try {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-    } catch (ClassNotFoundException e) {
-        System.err.println("找不到MySQL JDBC驱动程序");
-        e.printStackTrace();
-        return;
+public class Main {
+    public static void main(String[] args) {
+        // 注册MySQL驱动
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("找不到MySQL JDBC驱动程序");
+            e.printStackTrace();
+            return;
+        }
+        new EmployeeManagementSystem().setVisible(true);
     }
-    new EmployeeManagementSystem().setVisible(true);
 }
